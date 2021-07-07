c38a126985a89a86796e5010e224f82409af0c32
Filipe Kuhn <filipe_kuhn@hotmail.com>
2021-07-07 11:39:25 -0300

    Controllers

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using testeef.Data;
-using testeef.Models;
+using testeef.Models; 
 
 namespace testeef.Controllers
 {
